c72b1ae814366213561a8788f50de9d8fd13d58c
Eliezer Cabezas <66449381+Eliezer-09@users.noreply.github.com>
2024-08-03 13:41:56 -0500

    Creación de componentes y servicios.

--- Front end/ecommerce-app/src/app/app.module.ts
+++ Front end/ecommerce-app/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { CategoryListComponent } from './components/category-list/category-list.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { CartComponent } from './components/cart/cart.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    CategoryListComponent,
+    ProductListComponent,
+    CartComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule,
